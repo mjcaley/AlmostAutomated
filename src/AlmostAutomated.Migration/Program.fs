9ee2877e7320129ab37af6dbb70ccd01548e3f3c
Mike Caley <mike.caley@thomsonreuters.com>
2023-01-17 15:54:07 -0500

    Initial migration

---
+++ src/AlmostAutomated.Migration/Program.fs
@@ -0,0 +1,17 @@
+﻿open AlmostAutomated.Migration.MigrationRunner
+open Microsoft.Extensions.Configuration
+
+[<EntryPoint>]
+let main args =
+    let config = 
+        ConfigurationBuilder()
+            .AddCommandLine(args)
+            .AddEnvironmentVariables("ALMOST_")
+            .AddJsonFile("appsettings.json", true, true)
+            .AddJsonFile($"appsettings.Development.json", true, true)
+            .AddJsonFile($"appsettings.Production.json", true, true)
+            .Build()
+    let connectionString = config.GetConnectionString "Database"
+
+    migrate connectionString
+    0
