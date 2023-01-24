478ba68daa6d9fc7ba728151972146d160abaa23
Mike Caley <mike.caley@thomsonreuters.com>
2023-01-24 13:22:22 -0500

    API call successful

--- src/AlmostAutomated.Web/src/ApiClient.fs
+++ src/AlmostAutomated.Web/src/ApiClient.fs
@@ -15,29 +15,20 @@ let internal formatApiUrl apiBase paths = apiBase + String.concat "/" paths
 
 let apiBase = Config.variable "API_ROOT"
 
-let templateDecoder =
-    Decode.object
-    <| fun get ->
-        { Id = get.Required.Field "id" Decode.int64
-          Title = get.Required.Field "title" Decode.string
-          Description = get.Required.Field "description" Decode.string }
-
-let templatesDecoder = Decode.array templateDecoder
-
 let extras = Extra.empty |> Extra.withInt64
 
 let listTemplates apiBase =
     promise {
         let! templateResult =
-            Fetch.tryGet (formatApiUrl apiBase [ "templates" ], extra = extras, decoder = templatesDecoder)
+            Fetch.tryGet<_, array<TemplateDTO>>(formatApiUrl apiBase [ "templates" ], extra=extras)
 
         return
             match templateResult with
             | Ok t -> Success <| List.ofArray t
             | Error message ->
-                match message with
-                | DecodingFailed _ -> DecodeError
-                | FetchFailed response -> HttpError(response.Status, response.StatusText)
-                | NetworkError exc -> NetworkErr exc
-                | _ -> UnknownError
+               match message with
+               | DecodingFailed _ -> DecodeError
+               | FetchFailed response -> HttpError(response.Status, response.StatusText)
+               | NetworkError exc -> NetworkErr exc
+               | _ -> UnknownError
     }
--- tests/AlmostAutomated.Web.Tests/Tests.fs
+++ tests/AlmostAutomated.Web.Tests/Tests.fs
@@ -1,16 +1,22 @@
 module Tests
 
-open Xunit
-open FsUnit.Xunit
+open Fable.Mocha
 open AlmostAutomated.Web.ApiClient
 open AlmostAutomated.Core.DTO
 open Thoth.Json
 
-[<Fact>]
-let ``Decord of TemplateDTO`` () =
-    let expected: TemplateDTO = {Id=42L; Title="title"; Description="description"}
-    let decoded = Decode.fromString templateDecoder """{id: 42, title="title", description="description"}"""
+let apiTests =
+    testList "API tests" [
+        test "converts string to TemplateDTO" {
+            let expected: TemplateDTO = {Id=42L; Title="title"; Description="description"}
+            let decoded = Decode.fromString templateDecoder """{id: 42, title="title", description="description"}"""
 
-    match decoded with
-    | Ok template -> template |> should equal expected
-    | Error _ -> false |> should be True
+            Expect.isOk decoded "ok"
+            match decoded with
+            | Ok t -> Expect.equal t expected "equal"
+            | Error message -> Expect.isTrue false "error"
+        }
+
+    ]
+
+Mocha.runTests apiTests
