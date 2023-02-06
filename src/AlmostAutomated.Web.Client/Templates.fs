f6d77a8775ce1e5ba6648cdf05c5c17d1758e463
Mike Caley <mike.caley@thomsonreuters.com>
2023-02-06 16:12:24 -0500

    Switch to Belaro

---
+++ src/AlmostAutomated.Web.Client/Templates.fs
@@ -0,0 +1,67 @@
+﻿module AlmostAutomated.Web.Client.Templates
+
+open System.Net.Http
+open System.Net.Http.Json
+open Microsoft.AspNetCore.Components
+open Elmish
+open Bolero
+open Bolero.Html
+open Bolero.Remoting
+open AlmostAutomated.Core.DTO
+
+
+type Model =
+    {
+        Templates: TemplateDTO[]
+        ErrorMessage: string
+    }
+
+let initModel = { Templates=Array.empty; ErrorMessage="" }
+
+type Message =
+    | GetTemplates
+    | GotTemplates of templates: TemplateDTO[]
+    | Error of exn
+
+let update (httpClient: HttpClient) message model =
+    match message with
+    | GetTemplates -> 
+        let getTemplates () = httpClient.GetFromJsonAsync<TemplateDTO[]>("http://localhost:5268/api/templates")
+        let cmd = Cmd.OfTask.either getTemplates () GotTemplates Error
+        model, cmd
+    | GotTemplates templates ->
+        { model with Templates=templates }, Cmd.none
+    | Error exn -> { model with ErrorMessage=exn.Message }, Cmd.none
+
+let view model _ =
+    div {
+        attr.style "border: solid;"
+
+        h1 { "Templates" }
+        p { text $"Error: {model.ErrorMessage}" }
+        ul {
+            for template in model.Templates do
+                li { 
+                    concat {
+                        "Id:"
+                        string template.Id
+                        ", "
+                        "Title: "
+                        template.Title
+                        ", "
+                        "Description: "
+                        template.Description
+                    }
+                }
+        }
+    }
+
+type TemplatesComponent() =
+    inherit ProgramComponent<Model, Message>()
+    
+    [<Inject>]
+    member val HttpClient = Unchecked.defaultof<HttpClient> with get, set
+
+    override this.Program =
+        let update = update this.HttpClient
+        Program.mkProgram (fun _ -> initModel, Cmd.ofMsg GetTemplates) update view
